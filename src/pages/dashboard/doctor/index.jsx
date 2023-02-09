3cc9230ccfd8a2cedb0783b5c31c3c7b79ff016b
AniruddhaGawali <aniruddhagawali05@gmail.com>
2023-02-10 04:08:16 +0530

    udate grid

--- src/pages/dashboard/doctor/index.jsx
+++ src/pages/dashboard/doctor/index.jsx
@@ -5,42 +5,57 @@ import QRScanner from "../../../components/QRScanner";
 import Button from "../../../components/Button";
 
 import { DataGrid } from "@mui/x-data-grid";
-const columns = [
-  { field: "id", headerName: "ID", width: 70 },
-  { field: "firstName", headerName: "First name", width: 130 },
-  { field: "lastName", headerName: "Last name", width: 130 },
-  {
-    field: "age",
-    headerName: "Age",
-    type: "number",
-    width: 90,
-  },
-  {
-    field: "fullName",
-    headerName: "Full name",
-    description: "This column has a value getter and is not sortable.",
-    sortable: false,
-    width: 160,
-    valueGetter: (params) =>
-      `${params.row.firstName || ""} ${params.row.lastName || ""}`,
-  },
-];
 
-const rows = [
-  { id: 1, lastName: "Snow", firstName: "Jon", age: 35 },
-  { id: 2, lastName: "Lannister", firstName: "Cersei", age: 42 },
-  { id: 3, lastName: "Lannister", firstName: "Jaime", age: 45 },
-  { id: 4, lastName: "Stark", firstName: "Arya", age: 16 },
-  { id: 5, lastName: "Targaryen", firstName: "Daenerys", age: null },
-  { id: 6, lastName: "Melisandre", firstName: null, age: 150 },
-  { id: 7, lastName: "Clifford", firstName: "Ferrara", age: 44 },
-  { id: 8, lastName: "Frances", firstName: "Rossini", age: 36 },
-  { id: 9, lastName: "Roxie", firstName: "Harvey", age: 65 },
-];
+import { RxCross2 } from "react-icons/rx";
+import { style } from "@mui/system";
 
 const Doctor = () => {
   const [result, setResult] = React.useState("No result");
   const [isQRScannerOpen, setIsQRScannerOpen] = React.useState(false);
+  const [selected, setSelected] = React.useState("No result");
+  const [open, setOpen] = React.useState(false);
+
+  const rows = [
+    { id: 1, lastName: "Snow", firstName: "Jon", age: 35 },
+    { id: 2, lastName: "Lannister", firstName: "Cersei", age: 42 },
+    { id: 3, lastName: "Lannister", firstName: "Jaime", age: 45 },
+    { id: 4, lastName: "Stark", firstName: "Arya", age: 16 },
+    { id: 5, lastName: "Targaryen", firstName: "Daenerys", age: null },
+    { id: 6, lastName: "Melisandre", firstName: null, age: 150 },
+    { id: 7, lastName: "Clifford", firstName: "Ferrara", age: 44 },
+    { id: 8, lastName: "Frances", firstName: "Rossini", age: 36 },
+    { id: 9, lastName: "Roxie", firstName: "Harvey", age: 65 },
+  ];
+
+  const columns = [
+    { field: "id", headerName: "ID", width: 70 },
+    { field: "firstName", headerName: "First name", width: 130 },
+    { field: "lastName", headerName: "Last name", width: 130 },
+    {
+      field: "age",
+      headerName: "Age",
+      type: "number",
+      width: 90,
+    },
+    {
+      field: "action",
+      headerName: "Action",
+      width: 150,
+      renderCell: (params) => (
+        <button
+          className="button"
+          onClick={() => {
+            console.log(params.row);
+            setOpen(true);
+            setSelected(params.row);
+          }}
+        >
+          Open
+        </button>
+      ),
+    },
+  ];
+
   return (
     <>
       <Navbar />
@@ -54,10 +69,12 @@ const Doctor = () => {
         }}
       >
         <Button
-          title="Open Scanner"
+          title={isQRScannerOpen ? "Close QR Scanner" : "Open QR Scanner"}
           function={() => setIsQRScannerOpen(!isQRScannerOpen)}
         />
       </div>
+      <br />
+      <br />
       <div
         className={styles.QRScanner}
         style={{
@@ -74,7 +91,14 @@ const Doctor = () => {
       </div>
 
       <div className={styles.container}>
-        <div style={{ height: 400, width: "100%" }}>
+        <div
+          style={{
+            height: 400,
+            width: "100%",
+            marginTop: "20px",
+            padding: "20px",
+          }}
+        >
           <DataGrid
             rows={rows}
             columns={columns}
@@ -83,6 +107,15 @@ const Doctor = () => {
             checkboxSelection
           />
         </div>
+      </div>
+
+      <div className={styles.pop} style={{ display: open ? "flex" : "none" }}>
+        <RxCross2
+          size="2rem"
+          onClick={() => setOpen(false)}
+          className={styles.cross}
+        />
+        <h1>{selected.id}</h1>
       </div>
 
       <br />
