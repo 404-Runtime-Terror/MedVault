2ae02b65068d1517e8151ac58d6b1c82780eb71b
Freakyab <aryanbhisikar1@gmail.com>
2023-02-09 19:16:23 +0530

    update login

--- src/pages/login/index.jsx
+++ src/pages/login/index.jsx
@@ -6,6 +6,44 @@ import style from "./style.module.css";
 import { FcGoogle } from "react-icons/fc";
 
 const Login = () => {
+  // const [type , setType] = React.useState("Patient");
+  // async function getLogin(username, password) {
+  //   // get request to backend to check if user is valid or not
+
+  //   // set loading to true so that loader will be shown
+  //   setIsLoading(true);
+
+  //   // send the username and password to backend and check if user is valid or not
+  //   await axios
+  //     .get(
+  //       "medvault.up.railway.app/" +
+  //         username +
+  //         "&password=" +
+  //         password
+  //     )
+  //     .then((res) => {
+  //       console.log(res.data);
+  //       if (res.data.isLogin === true) {
+  //         // if user is valid then set isLogin to true and redirect to dashboard
+  //         notifySuccessfull();
+  //         setUsername("");
+  //         setPassword("");
+  //         props.setUserID(res.data.userID);
+  //         Router.push({
+  //           pathname: "/dashboard",
+  //         });
+  //       } else {
+  //         // if user is not valid then show error message
+  //         notifyUnSuccessfull("Wrong Username or Password");
+  //       }
+  //     })
+  //     .catch((err) => {
+  //       // if there is any error then show error message
+  //       notifyUnSuccessfull("Something went wrong");
+  //     });
+
+  //   // set loading to false so that loader will be hidden
+  // }
   return (
     <>
       <div className={`${style.loginWrapper} gradient`}>
@@ -20,8 +58,13 @@ const Login = () => {
         <div className={style.loginRight}>
           <form>
             <h1>Login</h1>
+            {/* {type} */}
             <input type="text" placeholder="Username" />
             <input type="password" placeholder="Password" />
+            <input type = "checkbox" onChange={handlechange}/>
+            <label>is Doctor</label>
+            <input type = "checkbox" onChange={handlechange}/>
+            <label>is Patient</label>
             <Button title="Login" style={{ width: "60%" }} />
             <div className="divider" style={{ width: "75%" }}></div>
             <div
@@ -34,6 +77,7 @@ const Login = () => {
                 height: "fit-content",
               }}
             >
+              
               <button className={`${style.other_login_icon} button`}>
                 {/* // google icon */}
                 <FcGoogle size={"30px"} />
