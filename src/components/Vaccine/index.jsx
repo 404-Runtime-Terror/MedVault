2039445aad73d3420975f1704de4ea7cbe8b15c8
Mohit Raut <rautmohit063@gmail.com>
2023-02-09 19:05:51 +0530

    Prescription & Vaccine text alignment added

--- src/components/Vaccine/index.jsx
+++ src/components/Vaccine/index.jsx
@@ -9,14 +9,15 @@ const Vaccine = (props) => {
   return (
     <>
       <div className={style.vaccineBox}>
-        <div className={style.boxBorder}></div>
         <div className={style.vaccineHeader}>
           <h2 style={{ color: "var(--primary-color)" }}>CovinShield</h2>
           <h3 style={{ color: "var(--primary-color)" }}>
             Fri 30 Jan 2022 12:3:12
           </h3>
+          <div className={style.locationLogo}>
           <HiLocationMarker className={style.hosName} />
           <h4 className={style.hosName}>5 Star Hospital,Nagpur</h4>
+          </div>
         </div>
         {/* <div className={style.vaccineBody}>
             <ul className={style.medicine}>
